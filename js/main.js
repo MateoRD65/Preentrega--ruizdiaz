fa58cdeec7d7fc78292b5742a8fdbfe1e6392f67
mruizdiaz <mateord65@gmail.com>
2022-11-03 20:41:34 -0300

    Implementacion del typed.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const type = new Typed(".typed", {
+	strings: [
+		'<i class="typed-text">Desarrollador Web</i>',
+		'<i class="typed-text">Desarrollador BackEnd</i>',
+		'<i class="typed-text">Desarrollador FrontEnd</i>',
+	],
+	typeSpeed: 75,
+	startDelay: 300,
+	smartBackspace: true,
+	shuffle: false,
+	backSpeed: 75,
+	backDelay: 1500,
+	loop: true,
+	loopCount: false,
+	cursorChar: "|",
+	showCursor: true,
+	contentType: "html",
+});
